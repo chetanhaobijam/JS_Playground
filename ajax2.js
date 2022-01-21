61deb92f5519ea9581c8c67893075243aa5dcef2
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2022-01-21 14:18:52 +0530

    Added AJAX Fn to call users.json

--- ajax2.js
+++ ajax2.js
@@ -1,5 +1,7 @@
 const user = document.querySelector("#user");
 const btn1 = document.querySelector("#btn1");
+const users = document.querySelector("#users");
+const btn2 = document.querySelector("#btn2");
 
 const loadUser = () => {
   let xhr = new XMLHttpRequest();
@@ -32,4 +34,39 @@ const loadUser = () => {
   xhr.send();
 };
 
+const loadUsers = () => {
+  let xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "users.json", true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      let users = JSON.parse(this.responseText);
+
+      let output = "";
+
+      for (let user of users) {
+        output +=
+          "<ul>" +
+          "<li>ID: " +
+          user.id +
+          "</li>" +
+          "<li>Name: " +
+          user.name +
+          "</li>" +
+          "<li>Email: " +
+          user.email +
+          "</li>" +
+          "</ul>";
+      }
+
+      document.querySelector("#users").innerHTML = output;
+      console.log(output);
+    }
+  };
+
+  xhr.send();
+};
+
 btn1.addEventListener("click", loadUser);
+btn2.addEventListener("click", loadUsers);
