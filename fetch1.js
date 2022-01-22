4e18b92e1ce651ada0732287e6fc2ff792f1c66e
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2022-01-22 14:14:54 +0530

    Fetching Data from JSON

--- fetch1.js
+++ fetch1.js
@@ -1,5 +1,7 @@
 const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
 const output1 = document.querySelector("#output1");
+const usersOutput = document.querySelector("#users-output");
 
 const getText = () => {
   fetch("sample.txt")
@@ -11,3 +13,26 @@ const getText = () => {
 };
 
 btn1.addEventListener("click", getText);
+
+const getUsers = () => {
+  fetch("users.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let output = "<h2>Users List</h2>";
+
+      data.forEach((user) => {
+        output += `
+        <ul>
+          <li>ID: ${user.id}</li>
+          <li>Name: ${user.name}</li>
+          <li>Email: ${user.email}</li>
+        </ul>
+        `;
+      });
+
+      usersOutput.innerHTML = output;
+    })
+    .catch((err) => console.log(err));
+};
+
+btn2.addEventListener("click", getUsers);
