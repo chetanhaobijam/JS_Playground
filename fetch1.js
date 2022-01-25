6ce37df4e4ff7ece704cc0dfcb911e85f364b843
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2022-01-25 13:52:01 +0530

    Upto Fetch:Adding Posts

--- fetch1.js
+++ fetch1.js
@@ -1,19 +1,21 @@
-const btn1 = document.querySelector("#btn1");
-const btn2 = document.querySelector("#btn2");
-const output1 = document.querySelector("#output1");
-const usersOutput = document.querySelector("#users-output");
+const textBtn = document.querySelector("#get-text");
+const usersBtn = document.querySelector("#get-users");
+const postsBtn = document.querySelector("#get-posts");
+const outputArea = document.querySelector("#output");
 
+// Get Text File
 const getText = () => {
   fetch("sample.txt")
     .then((res) => res.text())
     .then((data) => {
-      output1.innerText = data;
+      outputArea.innerText = data;
     })
     .catch((err) => console.log(err));
 };
 
-btn1.addEventListener("click", getText);
+textBtn.addEventListener("click", getText);
 
+// Get JSON Users Data
 const getUsers = () => {
   fetch("users.json")
     .then((res) => res.json())
@@ -30,9 +32,53 @@ const getUsers = () => {
         `;
       });
 
-      usersOutput.innerHTML = output;
+      outputArea.innerHTML = output;
     })
     .catch((err) => console.log(err));
 };
 
-btn2.addEventListener("click", getUsers);
+usersBtn.addEventListener("click", getUsers);
+
+// Get Posts Data From External API
+const getPosts = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      let output = "<h2>Posts</h2>";
+      data.forEach((post) => {
+        output += `
+        <div>
+          <h3>${post.title}</h3>
+          <p>${post.body}</p>
+        </div>
+        `;
+      });
+      outputArea.innerHTML = output;
+    })
+    .catch((err) => console.log(err));
+};
+
+postsBtn.addEventListener("click", getPosts);
+
+// Add New Posts
+const addPostForm = document.querySelector("#add-post");
+
+const addPost = (e) => {
+  e.preventDefault();
+
+  let title = document.querySelector("#title").value;
+  let body = document.querySelector("#body").value;
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      Accept: 'application/json, text/plain, "/"',
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ title: title, body: body }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
+
+addPostForm.addEventListener("submit", addPost);
