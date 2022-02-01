e7645cb2406370838071701d3d2bab4c1bf7b7e7
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2022-02-01 14:13:27 +0530

    Async JS Callbacks Learned

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
-// Synchronous Javascript
-console.log("I");
-console.log(" eat ");
-setTimeout(() => {
-  console.log(" ice cream ");
-}, 4000);
-console.log(" with a ");
-console.log(" spoon.");
+// Asynchronous Javascript Callbacks
+let stocks = {
+  fruits: ["banana", "apple", "lemon", "orange"],
+  liquid: ["water", "ice"],
+  holders: ["cup", "stick", "cone"],
+  toppings: ["chocolate", "peanuts"],
+};
+
+const order = (fruit_index, call_production) => {
+  // Select Fruits
+  setTimeout(() => {
+    console.log(`${stocks.fruits[fruit_index]} has been selected`);
+
+    call_production();
+  }, 2000);
+};
+
+const production = () => {
+  setTimeout(() => {
+    console.log("Production has started");
+    setTimeout(() => {
+      console.log("Fruit has been cut");
+      setTimeout(() => {
+        console.log(
+          `${stocks.liquid[0]} and ${stocks.liquid[1]} has been added`
+        );
+        setTimeout(() => {
+          console.log("Machine has started");
+          setTimeout(() => {
+            console.log(`Ice Cream has been put into a ${stocks.holders[1]}`);
+            setTimeout(() => {
+              console.log(
+                `${stocks.toppings[0]} has been selected as Ice Cream Toppings`
+              );
+              setTimeout(() => {
+                console.log("Ice Cream Served. Bon apetit!");
+              }, 2000);
+            }, 3000);
+          }, 2000);
+        }, 1000);
+      }, 1000);
+    }, 2000);
+  }, 0000);
+};
+
+// Trigger
+order(1, production);
